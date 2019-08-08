f3ca0cd6cd6c81d020dced5728131e8b0c6e1f63
Matt25969 <matt.jo.hunt@gmail.com>
2019-08-08 12:11:50 +0100

    Changed endpoint

--- src/main/java/io/tpd/kafkaexample/HelloKafkaController.java
+++ src/main/java/io/tpd/kafkaexample/HelloKafkaController.java
@@ -10,6 +10,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -36,16 +37,16 @@ public class HelloKafkaController {
         this.messagesPerRequest = messagesPerRequest;
     }
 
-    @GetMapping("/hello")
-    public String hello() throws Exception {
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name) throws Exception {
         latch = new CountDownLatch(messagesPerRequest);
         IntStream.range(0, messagesPerRequest)
                 .forEach(i -> this.template.send(topicName, String.valueOf(i),
-                        new PracticalAdvice("A Practical Advice", i))
+                        new PracticalAdvice("A Practical Advice "+name, i))
                 );
         latch.await(60, TimeUnit.SECONDS);
         logger.info("All messages received");
-        return "Hello Kafka!";
+        return "Hello "+name;
     }
 
     @KafkaListener(topics = "advice-topic", clientIdPrefix = "json",
