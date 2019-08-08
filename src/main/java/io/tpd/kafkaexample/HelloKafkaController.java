89c831a48df9f34b43a43954c0a553eeda32cbcf
Matt25969 <matt.jo.hunt@gmail.com>
2019-08-08 12:31:00 +0100

    Changed return

--- src/main/java/io/tpd/kafkaexample/HelloKafkaController.java
+++ src/main/java/io/tpd/kafkaexample/HelloKafkaController.java
@@ -42,9 +42,10 @@ public class HelloKafkaController {
     	String retVal = ""; 
         latch = new CountDownLatch(messagesPerRequest);
         IntStream.range(0, messagesPerRequest)
+        .forEach(i -> retVal+=String.valueOf(i))
                 .forEach(i -> this.template.send(topicName, String.valueOf(i),
-                        new PracticalAdvice("A Practical Advice "+name, i)))
-                .forEach(i -> retVal+=String.valueOf(i) );
+                        new PracticalAdvice("A Practical Advice "+name, i)));
+                
         latch.await(60, TimeUnit.SECONDS);
         logger.info("All messages received");
         return retVal;
