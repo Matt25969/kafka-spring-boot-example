ce89c1be7168873a85254df6e158361d89df7d85
Matt25969 <matt.jo.hunt@gmail.com>
2019-08-08 13:16:28 +0100

    Changed return

--- src/main/java/io/tpd/kafkaexample/HelloKafkaController.java
+++ src/main/java/io/tpd/kafkaexample/HelloKafkaController.java
@@ -39,16 +39,16 @@ public class HelloKafkaController {
 
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable String name) throws Exception {
-    	String retVal = ""; 
+    
         latch = new CountDownLatch(messagesPerRequest);
-        IntStream.range(0, messagesPerRequest).forEach(i -> retVal+=i)
+        IntStream.range(0, messagesPerRequest)
         
                 .forEach(i -> this.template.send(topicName, String.valueOf(i),
                         new PracticalAdvice("A Practical Advice "+name, i)));
                 
         latch.await(60, TimeUnit.SECONDS);
         logger.info("All messages received");
-        return retVal;
+        return name;
     }
 
     @KafkaListener(topics = "advice-topic", clientIdPrefix = "json",
